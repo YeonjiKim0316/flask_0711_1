92bbdf98e3e354d7e87e97c624158a23963bc57d
YeonjiKim0316 <73862452+YeonjiKim0316@users.noreply.github.com>
2023-07-11 16:48:52 +0900

    post_list.html에 값을 전달하는 post를 완성해주세요.

--- app/models.py
+++ app/models.py
@@ -11,10 +11,10 @@ class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
-    # Foreign Key로 걸려있는 테이블에서 삭제가 발생했을 때 
-        # 1) Answer도 같이 지운다
-        # 2) Answer는 남겨놓는다 
-            # Question의 id를 남겨놓는다
-            # Question의 id를 삭제한다
     question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
     question = db.relationship('Question', backref=db.backref('answer_set'))
+    # Foreign Key로 걸려있는 테이블에서 삭제가 발생했을 때 
+            # 1) Answer도 같이 지운다
+            # 2) Answer는 남겨놓는다 
+                # Question의 id를 남겨놓는다
+                # Question의 id를 삭제한다
--- app/views/basic_views.py
+++ app/views/basic_views.py
@@ -1,8 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from app.models import Question, Answer
 
                  # 우리가 부를 이름, flask 프레임워크가 찾을 이름, 라우팅주소
 fisa = Blueprint('basic', __name__, url_prefix='/fisa')
+
+@fisa.route('/detail/<int:question_id>/')
+def detail(question_id):
+    question = Question.query.get(question_id)
+    return render_template('question/question_detail.html', question=question)
+
+@fisa.route('/post')
+# def post_list():
+#     return 'question\qustion_list.html' 
+
 
 @fisa.route('/about_me')
 def about_me():
