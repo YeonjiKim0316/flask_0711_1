83641794dffcd9ad734bf36c3b20463ec92f25f9
YeonjiKim0316 <73862452+YeonjiKim0316@users.noreply.github.com>
2023-07-12 17:33:56 +0900

    answer form

--- app/views/answer_views.py
+++ app/views/answer_views.py
@@ -11,11 +11,18 @@ answer = Blueprint('answer', __name__, url_prefix='/answer')
 @answer.route('/create/<int:question_id>/', methods=['GET', 'POST'])
 def create(question_id):
     form = AnswerForm()
+
+    # question 화면 가져오기
+    question = Question.query.get_or_404(question_id)
+    
     if form.validate_on_submit():
         # 어느 글에서 오는지 (question_id)
         a = Answer(content=form.content.data, create_date=datetime.now())
+
+        # answer의 question_id 필드에 추가
+        # question.answer_set.append(answer)
         db.session.add(a)
         db.session.commit()
         # question으로 불리우는 basic_views.py의 detail 함수를 호출하는데 question_id를 함께 전달
         return redirect(url_for('question.detail', question_id=question_id))
-    return render_template('question/question_detail.html', question_id=question_id, form=form)
+    return render_template('question/question_detail.html', question=question, form=form)
