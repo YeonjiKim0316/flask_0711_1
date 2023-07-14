e8a92c4e44dc5e54d44fc9b285976e361afa27fe
YeonjiKim0316 <73862452+YeonjiKim0316@users.noreply.github.com>
2023-07-14 14:16:55 +0900

    로그인 기능 구현

--- app/views/auth_views.py
+++ app/views/auth_views.py
@@ -15,11 +15,14 @@ def signup():
     form = UserCreateForm()
     if request.method == 'POST' and form.validate_on_submit():
         # 이미 있는 id가 아닌지 확인
-        user = User.query.filter_by(username=form.username.data)
+        user = User.query.filter_by(username=form.username.data).first()
+        print(user)
         # db에 username이 없다면
         if not user:
             # form에서 받은 데이터로 user라는 객체를 만들어서
-            user = User(username=form.username.data, password=form.password1.data, email=form.email.data)
+            # password는 암호화
+            user = User(username=form.username.data, password=generate_password_hash(form.password1.data),
+                        email=form.email.data)
             # db의 user 테이블에 추가
             db.session.add(user)
             # db에 커밋
@@ -27,7 +30,7 @@ def signup():
             return redirect(url_for('basic.index'))
         else:
             flash('이미 존재하는 사용자입니다.')
-    return render_template('user/signup.html', form=form)
+    return render_template('auth/signup.html', form=form)
 
 
 # /login/ URL과 매핑되는 login 함수는 signup 함수와 비슷하게 동작합니다. POST 방식에는 로그인을 수행하고, GET 요청에는 로그인 화면을 보여줍니다.
@@ -36,10 +39,9 @@ def login():
     form = UserLoginForm()
     if request.method == 'POST' and form.validate_on_submit():
         error = None
-				# 폼 입력으로 받은 username으로 데이터베이스에 해당 사용자가 있는지를 검사한다. 만약 사용자가 없으면 "존재하지 않는 사용자입니다."라는 오류를 발생시키고, 사용자가 있다면 폼 입력으로 받은 password와 check_password_hash 함수를 사용하여 데이터베이스의 비밀번호와 일치하는지를 비교합니다.
+		# 폼 입력으로 받은 username으로 데이터베이스에 해당 사용자가 있는지를 검사한다. 만약 사용자가 없으면 "존재하지 않는 사용자입니다."라는 오류를 발생시키고, 사용자가 있다면 폼 입력으로 받은 password와 check_password_hash 함수를 사용하여 데이터베이스의 비밀번호와 일치하는지를 비교합니다.
         user = User.query.filter_by(username=form.username.data).first()
-        
-        # 로그인 자체가 막혀림 
+        # 로그인 자체가 막혀버림 
         if not user:
             error = "존재하지 않는 사용자입니다."
         elif not check_password_hash(user.password, form.password.data):
@@ -64,26 +66,26 @@ def login():
     return render_template('auth/login.html', form=form)
 
 
-@auth.before_app_request
-def load_logged_in_user():
-    user_id = session.get('user_id')
-    if user_id is None:
-        g.user = None
-    else:
-        g.user = User.query.get(user_id)
+# @auth.before_app_request
+# def load_logged_in_user():
+#     user_id = session.get('user_id')
+#     if user_id is None:
+#         g.user = None
+#     else:
+#         g.user = User.query.get(user_id)
 
 
-@auth.route('/logout/')
-def logout():
-    session.clear()
-    return redirect(url_for('basic.index'))
+# @auth.route('/logout/')
+# def logout():
+#     session.clear()
+#     return redirect(url_for('basic.index'))
 
 
-def login_required(view):
-    @functools.wraps(view)
-    def wrapped_view(*args, **kwargs):
-        if g.user is None:
-            _next = request.url if request.method == 'GET' else ''
-            return redirect(url_for('auth.login', next=_next))
-        return view(*args, **kwargs)
-    return wrapped_view
+# def login_required(view):
+#     @functools.wraps(view)
+#     def wrapped_view(*args, **kwargs):
+#         if g.user is None:
+#             _next = request.url if request.method == 'GET' else ''
+#             return redirect(url_for('auth.login', next=_next))
+#         return view(*args, **kwargs)
+#     return wrapped_view
