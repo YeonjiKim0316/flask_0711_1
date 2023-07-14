187abc23f0462dcc0f7af10355fe7bf13dbc14c1
YeonjiKim0316 <73862452+YeonjiKim0316@users.noreply.github.com>
2023-07-14 15:03:56 +0900

    change Question Model

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,19 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import config 
+from sqlalchemy import MetaData
 
-db = SQLAlchemy()
+
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
+
 migrate = Migrate()
 
 def create_app():
@@ -12,7 +23,11 @@ def create_app():
     # ORM에 대한 설정
     app.config.from_object(config)
     db.init_app(app)
-    migrate.init_app(app, db)
+    # migrate.init_app(app, db)
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
 
     from .views import basic_views, answer_views, question_views, auth_views
     app.register_blueprint(basic_views.fisa)
--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ class Question(db.Model):
     subject = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    user = db.relationship('User', backref=db.backref('question_set'))
 
 class Answer(db.Model):
     # 답변의 고유번호 - 숫자 PK다 : 다음번호 자동 생성
